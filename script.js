b600136c92c4ef959328e4ba73a259253c6a5e72
Desmond Yim <desmond.y01@gmail.com>
2021-11-26 21:43:04 -0700

    Add error condition if input is not appropriate

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ function computerPlay() {
 
 function play(playerSelection, computerSelection) {
     console.log("You chose", playerSelection, "and the Computer chose", computerSelection);
-    if ((playerSelection === "Paper" && computerSelection === "Rock") ||
-            (playerSelection === "Rock" && computerSelection === "Scissors") ||
-            (playerSelection === "Scissors" && computerSelection === "Paper")) {
+    if (playerSelection.toUpperCase() != "rock".toUpperCase() &&
+            playerSelection.toUpperCase() != "paper".toUpperCase() &&
+            playerSelection.toUpperCase() != "scissors".toUpperCase()) {
+        console.log("Error! Please input a valid answer")
+    } else if ((playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase()) ||
+            (playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
+            (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase())) {
         console.log("You win!", playerSelection, "beats", computerSelection);
-    } else if (playerSelection === computerSelection) {
+    } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
         console.log("It's a tie, both chose", playerSelection);
     }
         else {
