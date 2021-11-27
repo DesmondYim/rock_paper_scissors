02eddff625d9db874eb493fa14c8cc2db3daaee8
Desmond Yim <desmond.y01@gmail.com>
2021-11-26 22:24:23 -0700

    Add loop for game rounds

--- script.js
+++ script.js
@@ -6,46 +6,33 @@ function computerPlay() {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toUpperCase() != "rock".toUpperCase() &&
+function playGame(playerSelection, computerSelection) {
+    for (let i = 0; i < 5; i++) {
+        function playRound();
+    }
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toUpperCase() != "rock".toUpperCase() &&
             playerSelection.toUpperCase() != "paper".toUpperCase() &&
             playerSelection.toUpperCase() != "scissors".toUpperCase()) {
-        return `You chose ${playerSelection} and the Computer chose ${computerSelection} 
+            return `You chose ${playerSelection} and the Computer chose ${computerSelection} 
             Error! Please input a valid choice!`;
-    } else if ((playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase()) ||
+        } else if ((playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase()) ||
             (playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
             (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase())) {
-        return `You chose ${playerSelection} and the Computer chose ${computerSelection}. 
+            return `You chose ${playerSelection} and the Computer chose ${computerSelection}. 
             You win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        return `You chose ${playerSelection} and the Computer chose ${computerSelection}.
+        } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+            return `You chose ${playerSelection} and the Computer chose ${computerSelection}.
             It's a tie, both chose ${playerSelection}`;
-    }
+        }
         else {
-        return `You chose ${playerSelection} and the Computer chose ${computerSelection}.
+            return `You chose ${playerSelection} and the Computer chose ${computerSelection}.
             You lose! ${computerSelection} beats ${playerSelection}`;
+        }
     }
 }
 
     const playerSelection = playerChoice;
     const computerSelection = computerPlay();
     console.log(playRound(playerSelection, computerSelection));
-
-//     console.log("You chose", playerSelection, "and the Computer chose", computerSelection);
-//     if (playerSelection.toUpperCase() != "rock".toUpperCase() &&
-//             playerSelection.toUpperCase() != "paper".toUpperCase() &&
-//             playerSelection.toUpperCase() != "scissors".toUpperCase()) {
-//         console.log("Error! Please input a valid answer")
-//     } else if ((playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase()) ||
-//             (playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
-//             (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase())) {
-//         console.log("You win!", playerSelection, "beats", computerSelection);
-//     } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-//         console.log("It's a tie, both chose", playerSelection);
-//     }
-//         else {
-//         console.log("You lose!", computerSelection, "beats", playerSelection);
-//     }
-// }
-
-// play(playerChoice, computerPlay())
