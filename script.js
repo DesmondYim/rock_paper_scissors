ca9707cc486e38d689c77ae1f742a560f69efd9c
Desmond Yim <desmond.y01@gmail.com>
2021-11-26 21:12:54 -0700

    Create win scenario

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choice = prompt("Rock, paper, or scissors?");
+let playerChoice = prompt("Rock, paper, or scissors?");
 
 function computerPlay() {
     let computerChoices = ['Rock', 'Paper', 'Scissors'];
@@ -6,4 +6,10 @@ function computerPlay() {
     return computerChoice;
 }
 
-console.log(computerPlay())
+function play(playerSelection, computerSelection) {
+    if ((playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Rock" && computerChoice === "Scissors") ||
+        (playerChoice === "Scissors" && computerChoice === "Paper") {
+        console.log ("You win!", playerSelection, "beats", computerSelection)
+    }
+}
