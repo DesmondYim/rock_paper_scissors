19a6c5b4cfcb8f25ee407fe3614081446accb40f
Desmond Yim <desmond.y01@gmail.com>
2021-11-29 20:01:16 -0700

    Add loop with final scoring

--- script.js
+++ script.js
@@ -1,63 +1,67 @@
 let choices = ['rock', 'paper', 'scissors'];
 
 function playerChoose() {
-    let playerChoice = prompt("Rock, paper, or scissors?");
-    return playerChoice;
+    // return playerChoice = prompt("Rock, paper, or scissors?"); // not working in loop
+    return prompt("Rock, paper, or scissors?");
 }
 
 function computerPlay() {
     let computerChoices = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    return computerChoice;
+    // let computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)]; // not working in loop
+    // return computerChoice;
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)]
 }
 
-const playerChoice = playerChoose();
-const computerChoice = computerPlay();
+const results = ['error', 'win', 'loss', 'tie']
   
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() != "rock".toUpperCase() &&
             playerSelection.toUpperCase() != "paper".toUpperCase() &&
             playerSelection.toUpperCase() != "scissors".toUpperCase()) {
-        return error = `You chose ${playerSelection} and the Computer chose ${computerSelection} 
+        return results[0] = `You chose ${playerSelection} and the Computer chose ${computerSelection} 
             Error! Please input a valid choice!`;
     } else if ((playerSelection.toUpperCase() === "Paper".toUpperCase() && computerSelection.toUpperCase() === "Rock".toUpperCase()) ||
             (playerSelection.toUpperCase() === "Rock".toUpperCase() && computerSelection.toUpperCase() === "Scissors".toUpperCase()) ||
             (playerSelection.toUpperCase() === "Scissors".toUpperCase() && computerSelection.toUpperCase() === "Paper".toUpperCase())) {
-        return win = `You chose ${playerSelection} and the Computer chose ${computerSelection}. 
+        return results[1] = `You chose ${playerSelection} and the Computer chose ${computerSelection}. 
             You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        return tie = `You chose ${playerSelection} and the Computer chose ${computerSelection}.
+        return results[3] = `You chose ${playerSelection} and the Computer chose ${computerSelection}.
             It's a tie! Both chose ${playerSelection}`;
     }
     else {
-        return loss = `You chose ${playerSelection} and the Computer chose ${computerSelection}.
+        return results[2] = `You chose ${playerSelection} and the Computer chose ${computerSelection}.
             You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
-console.log(playRound(playerChoice, computerChoice));
-// // console.log(playGame(playerSelection, computerSelection));
-    
-// function playGame() {
-//     // for each round out of 5
-//     const playerScore = 0;
-//     const computerScore = 0;
-//     for (let i = 0; i <= 5; i++) {
-//         // play a round of Rock paper scissors
-//         console.log(playRound());
-//         // Add point to winner of round if winner available
-//         // splitResults = playRound
-//         // if ()
 
-//         // console.log(playRound(playerSelection, computerSelection));
-//     } 
-// }
-    // if playround is win
-    //     then + 1 for me
-    // else loss
-    // then + 1 for computer
-    // report score "Game end! You scored ___, and the computer scored ___"
-    
-    // for (let i = 0; i < 5; i++) {
-    //     console.log(playRound(playerSelection,computerSelection));
+let playerScore = 0;
+let computerScore = 0;
 
-// console.log(playGame());
+function playGame() {
+    let gameRound = 0
+    while (gameRound < 5) {
+
+        let playerChoice = playerChoose();
+        let computerChoice = computerPlay();
+        
+        console.log(playRound(playerChoice, computerChoice));
+        if (playRound(playerChoice, computerChoice) == results[0]) {
+            console.log("Try again");
+        } else if (playRound(playerChoice, computerChoice) == results[1]) {
+            playerScore++;
+        } else if (playRound(playerChoice, computerChoice) == results[2]) {
+            computerScore++;
+        } else {
+            console.log(`No points for both!`)
+        }
+        gameRound++;
+    } 
+}
+playGame();
+if(playerScore > computerScore) {
+    console.log("You win!");
+} else {
+    console.log("You lose!");
+}
+console.log(`Your Score: ${playerScore}     Computer Score: ${computerScore}`);
